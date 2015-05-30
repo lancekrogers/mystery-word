bdfdc26a2860a8367d23b80b6813635de9b7cdfa
Lance Rogers <lancekincaid1994@gmail.com>
2015-05-30 18:31:31 -0400

    creating game function

--- mystery_word.py
+++ mystery_word.py
@@ -11,23 +11,35 @@ def user_instruct():
     If you guess a letter contained in\n
     the word that will not count against\n
     you.\n
-    Have fun and enjoy Mystery Word!
-    ''')
-    pass
+    Have fun and enjoy Mystery Word!\n
+    Press RETURN to continue.\n''')
 
 
-# pull_words() function takes a word from a dicionary and passes a random word
+# pull_words() function takes a word from a file and passes a random word
 def pull_words():
     with open("/usr/share/dict/words") as dictionary:
         dictionary = dictionary.read().split()
         random_word = random.choice(dictionary)
         return random_word
 
-random_word = pull_words()
 
-
+# takes in random word and outputs the number of letters in that word
 def random_word_length(random_word):
     letters_in_word = len(random_word)
     print("Your word contains {} letters".format(letters_in_word))
-    pass
+    return letters_in_word
+
+
+# put random_word into a list of single letter strings
+# use indexes to display the guessed letters in the
+# proper order and "_" for the letters that havent been
+# guessed yet
+def game_function(random_word):
+    letter_list = [x for x in random_word]
+    print(letter_list)
+
+
+user_instruct()
+random_word = pull_words()
 random_word_length(random_word)
+game_function(random_word)
